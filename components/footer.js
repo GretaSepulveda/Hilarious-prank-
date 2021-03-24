2dbd6494315f0e29b6a4ad87ca9883ac6388b1b8
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-03-24 11:56:58 -0500

    updated border-top

--- components/footer.js
+++ components/footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
           footer {
             width: 100%;
             height: 100px;
-            border-top: 2px solid #eaeaea;
+            border-top: 4px solid #eaeaea;
             display: flex;
             justify-content: center;
             align-items: center;
